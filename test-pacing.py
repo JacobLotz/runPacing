5832d4968be41775c5b41e898590d47ca13bf94e
Jacob Lotz <j.e.lotz@tudelft.nl>
2023-03-30 11:36:38 +0200

    Add python testing file

--- test-pacing.py
+++
@@ -1,8 +0,0 @@
-def test_roundMicroseconds(time):
-
-
-def test_calcPacing(dist, hours = 0, minutes = 0, seconds = 0, printresult = True):
-
-
-
-def test_calcRevPacing(dist, minutes = 0, seconds = 0, printresult = True):
